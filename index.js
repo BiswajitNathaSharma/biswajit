dde75cfd8f4e6bda6a18d0392f19835ffae05f24
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-02-28 01:09:56 +0530

    adjustment

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ const miniProjects = [
 window.addEventListener('load', ()=>{
     document.body.style.overflow =  'hidden';
     const loader=document.querySelector('.loader-container');
-    setTimeout(() => {
     loader.style.display='none';
+    setTimeout(() => {
     document.querySelector(".moving-image").classList.add("move");
     document.querySelector(".main-left").classList.add("move-left");
     document.body.style.overflow =  'visible';
