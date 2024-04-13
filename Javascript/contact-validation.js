81a40cb414a973aa957bc38a0cd17c0c35f29ba9
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2024-04-13 18:21:12 +0530

    task to fix the popup

--- Javascript/contact-validation.js
+++ Javascript/contact-validation.js
@@ -5,12 +5,19 @@ const userEmail = document.getElementById("email");
 const userMessage = document.getElementById("message");
 const errorSpace = document.getElementById("error-message");
 
-const error = document.querySelector(".error")
+const error = document.querySelector(".error");
 const nameError = document.querySelector(".name-error");
 const emailError = document.querySelector(".email-error");
 const subjectError = document.querySelector(".subject-error");
 const messageError = document.querySelector(".message-error");
 
+const popup = document.getElementById("popup");
+const popup_p = document.getElementById("popup-p");
+const popup_h = document.getElementById("popup-h2")
+
+const overlay = document.querySelector(".overlay");
+
+// form validation start
 // validate name start 
 function validateName() {
     const regex = /^[a-zA-Z]+(?:[\s.]+[a-zA-Z]+)*$/;
@@ -31,17 +38,17 @@ function validateName() {
 }
 
 // subject validation start
-function validateSubject(){
-    const subject  = userSubject.value.trim();
+function validateSubject() {
+    const subject = userSubject.value.trim();
 
     let required = 10;
     const nonWhitespaceChars = subject.replace(/\s/g, '').length;
-    let left  = required - nonWhitespaceChars;
+    let left = required - nonWhitespaceChars;
 
     let maxi = 25;
     let overChar = subject.length - maxi;
 
-    if(subject.length === 0 || nonWhitespaceChars < required){
+    if (subject.length === 0 || nonWhitespaceChars < required) {
         subjectError.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>`;
         userSubject.style.border = "2px solid rgb(164, 0, 0)"
         errorSpace.innerText = `${left} more characters required for a valid subject`;
@@ -85,8 +92,8 @@ function validateMessage() {
     const message = userMessage.value;
     let required = 35;
     const nonWhitespaceChars = message.replace(/\s/g, '').length;
-    let left  = required - nonWhitespaceChars;
-    if (message.length < required || message.length === 0 ) {
+    let left = required - nonWhitespaceChars;
+    if (message.length < required || message.length === 0) {
         errorSpace.innerHTML = `${left} more characters required to send the message`;
         userMessage.style.border = "2px solid rgb(164, 0, 0)"
         messageError.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>`;
@@ -96,29 +103,56 @@ function validateMessage() {
         errorSpace.innerHTML = "<span style='color:rgb(41, 122, 1)'>Now message can be send</span>";
         userMessage.style.border = "2px solid rgb(27, 115, 1)"
         messageError.innerHTML = `<i class="fa-solid fa-circle-check" style='color:rgb(41, 122, 1)'></i>`;
-        
+
         return true;
     }
 }
 
 // smtp intigration start 
-function sendmail(){
+function sendmail() {
     const messageBody = `Hi I am ${userName.value} <br> My email address is: ${userEmail.value} <br> My message is: ${userMessage.value}`
     Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "sharmaoncourse@gmail.com",
-    Password : "B33D9F91BB922C2DC7D2B52C77A1E9BF2F68",
-    To : 'lipubiswajit109@gmail.com',
-    From : "sharmaoncourse@gmail.com",
-    Subject : userSubject.value,
-    Body : messageBody
-}).then(    
-    (message) => alert(message)
-);}
+        Host: "smtp.elasticemail.com",
+        Username: "sharmaoncourse@gmail.com",
+        Password: "B33D9F91BB922C2DC7D2B52C77A1E9BF2F68",
+        To: 'lipubiswajit109@gmail.com',
+        From: "sharmaoncourse@gmail.com",
+        Subject: userSubject.value,
+        Body: messageBody
+    }).then(
+        (message) => openPopup(message)
+    );
+}
 
-function validateAndSubmit(e){
+function openPopup(message) {
+    if (message.includes("OK")) {
+        popup_p.innerText = "Message has successfully send to admin."
+    }
+    else {
+        popup_h.innerText = `Sorry!`
+        popup_p.innerText = `${message}`
+    }
+    overlay.style.display = "block";
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+    form.reset();
+    userName.innerText = ``;
+    userSubject.innerText = ``;
+    userEmail.innerText = ``;
+    userMessage.innerText = ``;
+    errorSpace.innerText = ``;
+    nameError.innerText = ``;
+    emailError.innerText = ``;
+    subjectError.innerText = ``;
+    messageError.innerText = ``;
+    overlay.style.display = "none";
+}
+function validateAndSubmit(e) {
     e.preventDefault();
-    if(!validateName() || !validateSubject() || !validateEmail() || !validateMessage()){
+    if (!validateName() || !validateSubject() || !validateEmail() || !validateMessage()) {
         errorSpace.innerText = `Please fix the error to submit the credentials`;
         setTimeout(() => {
             errorSpace.innerText = ''
